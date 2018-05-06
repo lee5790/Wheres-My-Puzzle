61b9f0c90efb5eadd7f64deec9e3c61b1f962e3a
mpodczerwinski <mpodczerwinski@gmail.com>
2018-05-06 14:51:45 -0400

    Finished writing activeHuntFragment. UNTESTED

--- app/src/main/java/edu/rosehulman/podczemd/wheres_my_puzzle/Fragments/ActiveHuntFragment.java
+++ app/src/main/java/edu/rosehulman/podczemd/wheres_my_puzzle/Fragments/ActiveHuntFragment.java
@@ -6,11 +6,14 @@ import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,14 +21,18 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.text.DecimalFormat;
 
 import edu.rosehulman.podczemd.wheres_my_puzzle.Interfaces.LocationObserver;
 import edu.rosehulman.podczemd.wheres_my_puzzle.Interfaces.LocationSource;
 import edu.rosehulman.podczemd.wheres_my_puzzle.Interfaces.ViewChanger;
+import edu.rosehulman.podczemd.wheres_my_puzzle.Models.Hint;
+import edu.rosehulman.podczemd.wheres_my_puzzle.Models.Hunt;
 import edu.rosehulman.podczemd.wheres_my_puzzle.Models.User;
 import edu.rosehulman.podczemd.wheres_my_puzzle.R;
 
+import static edu.rosehulman.podczemd.wheres_my_puzzle.MainActivity.ARG_HUNT;
 import static edu.rosehulman.podczemd.wheres_my_puzzle.MainActivity.ARG_USER;
 
 /**
@@ -33,6 +40,7 @@ import static edu.rosehulman.podczemd.wheres_my_puzzle.MainActivity.ARG_USER;
  */
 public class ActiveHuntFragment extends Fragment implements LocationObserver, OnMapReadyCallback {
 
+    private static final double ACCEPTABLE_DISTANCE_FROM_GOAL = 10;
     private User user;
     private ViewChanger viewChanger;
     private LocationSource locationSource;
@@ -46,16 +54,19 @@ public class ActiveHuntFragment extends Fragment implements LocationObserver, On
     private GoogleMap map;
 
     private boolean firstUpdate;
+    private LatLng currentLatLing;
+    private Hunt hunt;
 
 
     public ActiveHuntFragment() {
         // Required empty public constructor
     }
 
-    public static CurrentHuntsFragment newInstance(User user) {
+    public static CurrentHuntsFragment newInstance(User user, Hunt hunt) {
         CurrentHuntsFragment fragment = new CurrentHuntsFragment();
         Bundle args = new Bundle();
         args.putParcelable(ARG_USER, user);
+        args.putParcelable(ARG_HUNT, hunt);
         fragment.setArguments(args);
         return fragment;
     }
@@ -66,6 +77,7 @@ public class ActiveHuntFragment extends Fragment implements LocationObserver, On
         firstUpdate = true;
         if (getArguments() != null) {
             user = getArguments().getParcelable(ARG_USER);
+            hunt = getArguments().getParcelable(ARG_HUNT);
         }
         if (mapView != null) {
             mapView.onCreate(savedInstanceState);
@@ -110,17 +122,70 @@ public class ActiveHuntFragment extends Fragment implements LocationObserver, On
         checkLocationButton = view.findViewById(R.id.checkLocationButton);
         previousHintsButton = view.findViewById(R.id.previousHintsButton);
         titleText = view.findViewById(R.id.activeHuntTitleTextView);
+        titleText.setText(hunt.getTitle());
         hintText = view.findViewById(R.id.activeHuntHintTextView);
         mapView = view.findViewById(R.id.activeHuntMapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
-
+        checkLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkLocation();
+            }
+        });
+        updateHintText();
         return view;
+    }
+
+    private void checkLocation() {
+        Hint currentHint = hunt.getHints().get(hunt.getCurrentHint());
+        double distanceFromGoal =  CalculationByDistance(currentLatLing,new LatLng(currentHint.getLatitude(),currentHint.getLongitude()));
+        if(distanceFromGoal < ACCEPTABLE_DISTANCE_FROM_GOAL){
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(currentHint.getFinishMessage());
+            builder.create().show();
+            hunt.incCurrentHint();
+            updateHintText();
+        }
+        else{
+            Toast.makeText(getContext(),"Wrong Location",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void updateHintText() {
+        Hint currentHint = hunt.getHints().get(hunt.getCurrentHint());
+        hintText.setText(currentHint.getHint());
+    }
+
+    public double CalculationByDistance(LatLng StartP, LatLng EndP) {
+        int Radius = 6371;// radius of earth in Km
+        double lat1 = StartP.latitude;
+        double lat2 = EndP.latitude;
+        double lon1 = StartP.longitude;
+        double lon2 = EndP.longitude;
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(lat1))
+                * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2)
+                * Math.sin(dLon / 2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double valueResult = Radius * c;
+        double km = valueResult / 1;
+        DecimalFormat newFormat = new DecimalFormat("####");
+        int kmInDec = Integer.valueOf(newFormat.format(km));
+        double meter = valueResult % 1000;
+        int meterInDec = Integer.valueOf(newFormat.format(meter));
+        Log.i("Radius Value", "" + valueResult + "   KM  " + kmInDec
+                + " Meter   " + meterInDec);
+
+        return Radius * c;
     }
 
     @Override
     public void updateLocation(Location location) {
-        LatLng currentLatLing = new LatLng(location.getLatitude(), location.getLongitude());
+        currentLatLing = new LatLng(location.getLatitude(), location.getLongitude());
+        map.clear();
         map.addCircle(new CircleOptions().center(currentLatLing).radius(3));
         if(firstUpdate) {
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLing, 17));
@@ -135,13 +200,6 @@ public class ActiveHuntFragment extends Fragment implements LocationObserver, On
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
         map.getUiSettings().setZoomControlsEnabled(true);
-        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                map.clear();
-                map.addMarker(new MarkerOptions().position(latLng));
-            }
-        });
     }
 
     @Override
